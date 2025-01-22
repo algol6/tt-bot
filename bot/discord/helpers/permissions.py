4a41915343a62210d10314c10e1c2de016a3bc92
Vice <vincenzoassenza360@gmail.com>
2025-01-22 21:22:01 +0100

    Create permissions.py

---
+++ bot/discord/helpers/permissions.py
@@ -0,0 +1,32 @@
+import functools
+from discord import ApplicationContext
+from bot.db import Database, Collection
+from bot.api import SMMOApi
+from bot.discord.helpers import command_utils
+
+
+
+async def is_admin_or_staff(ctx: ApplicationContext) -> bool:
+    user = await Database.select_one(Collection.USER.value, {"discord_id":ctx.author.id})
+    if user["smmo_id"] == 1030340:
+        return True
+    if user is None:
+        return False
+    game_users = await SMMOApi.get_guild_members(int(command_utils.get_config()["DEFAULT"]["guild_id"]))
+    return any(user["smmo_id"] == x.user_id and x.position != "Member" for x in game_users)
+
+def require_admin_or_staff():
+    def wrapper(func):
+        @functools.wraps(func)
+        async def wrapped(*args, **kwargs):
+            if len(args) >= 2 and isinstance(args[1], ApplicationContext):
+                ctx: ApplicationContext = args[1]
+                if not await is_admin_or_staff(ctx):
+                    await ctx.respond(
+                        content="You do not have permission to run this command",
+                        ephemeral=True
+                    )
+                    return
+            return await func(*args, **kwargs)
+        return wrapped
+    return wrapper
