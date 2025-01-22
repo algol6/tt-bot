b852ac214a97523911c8cb73c436b2f5c3880382
Vice <vincenzoassenza360@gmail.com>
2025-01-22 21:21:46 +0100

    Update _client.py

--- bot/discord/_client.py
+++ bot/discord/_client.py
@@ -1,27 +1,22 @@
-from os import getenv
 import discord
+from os import getenv
 from pycord.multicog import Bot
-from bot.db import Database
-import asyncio
 
 intent = discord.Intents.default()
-client = Bot(intents=intent, activity=discord.Activity(name=getenv("DISCORD_ACTIVITY"), type=discord.ActivityType.playing))
+client = Bot(intents=intent, activity=discord.Activity(name=getenv("DISCORD_ACTIVITY"), type=discord.ActivityType.watching))
 
 client.load_extension("bot.discord.modules.command_groups")
 client.load_extension("bot.discord.modules.admin")
+client.load_extension("bot.discord.modules.tt")
 
-@client.event
-async def on_application_command_error(ctx: discord.ApplicationContext, error: discord.DiscordException):
-    await ctx.respond("Unexpected error. Try again later.\nIf keep getting this error please report to Algol https://discord.gg/bPRJkpnySB")
-    from datetime import datetime
-    if ctx.guild is not None:
-        print(f"[{datetime.now()}]ERROR from: [{ctx.guild.name}] #{ctx.channel.name}")
-    print(error)
+# @client.event
+# async def on_application_command_error(ctx: discord.ApplicationContext, error: discord.DiscordException):
+#     await ctx.respond("Unexpected error. Try again later.")
+#     from datetime import datetime
+#     if ctx.guild is not None:
+#         print(f"[{datetime.now()}]ERROR from: #{ctx.channel.name}")
+#     print(error)
 
 
 def run() -> None:
-    try:
-        asyncio.run(Database.create_table())
-    except Exception as e:
-        print(e)
     client.run(getenv("DISCORD_TOKEN"))
