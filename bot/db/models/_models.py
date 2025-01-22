3b0c9c8dee2e1ef83108c98473429b219cbb80a0
Vice <vincenzoassenza360@gmail.com>
2025-01-22 21:21:35 +0100

    Create _models.py

---
+++ bot/db/models/_models.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass, asdict, field
+
+@dataclass
+class MongoDbDataclass:
+    _id: str
+    def as_dict(self):
+        return {key: value for key, value in asdict(self).items() if value is not None}
+
+@dataclass
+class User(MongoDbDataclass):
+    ign:str
+    discord_id:int
+    smmo_id:int
+    ett:int
+    btt:int
+    daily:bool
+    weekly:bool
+    monthly:bool
+    
+@dataclass
+class GameStats(MongoDbDataclass):
+    smmo_id:int
+    steps:int
+    npc:int
+    pvp:int
+    year:int
+    month:int
+    day:int
+    
+@dataclass
+class GameLbReward(MongoDbDataclass):
+    daily:bool
+    weekly:bool
+    monthly:bool
