b85744a5a67f1aa7d3e91d3c4f74c25763893c92
Vice <vincenzoassenza360@gmail.com>
2025-01-23 11:26:40 +0100

    improved db management

--- bot/db/_database.py
+++ bot/db/_database.py
@@ -4,6 +4,9 @@ from os import getenv
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.server_api import ServerApi
 from enum import Enum
+import asyncio
+
+sem = asyncio.Semaphore(1)
 
 class Collection(Enum):
     USER = "user"
@@ -12,39 +15,40 @@ class Collection(Enum):
 
 class Database:
     uri:str = getenv("DATABASE_URI")
+
+    @staticmethod
+    async def _get_db() -> AsyncIOMotorClient:
+        async with sem:
+            client = AsyncIOMotorClient(Database.uri, server_api=ServerApi('1'))
+            return client.ttdb
+
     @staticmethod
     async def insert_one(collection:str,obj:dict):
-        client = AsyncIOMotorClient(Database.uri, server_api=ServerApi('1'))
-        db = client.ttdb
+        db = await Database._get_db()
         await db[collection].insert_one(obj)
 
     @staticmethod
     async def insert(collection:str,objs:list[dict]):
-        client = AsyncIOMotorClient(Database.uri, server_api=ServerApi('1'))
-        db = client.ttdb
+        db = await Database._get_db()
         return await db[collection].insert_many([x for x in objs])
 
     @staticmethod
     async def select_one(collection:str,obj:dict):
-        client = AsyncIOMotorClient(Database.uri, server_api=ServerApi('1'))
-        db = client.ttdb
+        db = await Database._get_db()
         return await db[collection].find_one(obj)
     
     @staticmethod
     async def select(collection:str):
-        client = AsyncIOMotorClient(Database.uri, server_api=ServerApi('1'))
-        db = client.ttdb
-        cursor = db[collection].find()
-        return cursor.to_list()
+        db = await Database._get_db()
+        cursor = db[collection].find({})
+        return await cursor.to_list()
     
     @staticmethod
     async def update_one(collection:str, old_obj:dict, new_obj:dict):
-        client = AsyncIOMotorClient(Database.uri, server_api=ServerApi('1'))
-        db = client.ttdb
+        db = await Database._get_db()
         await db[collection].update_one({"_id":old_obj["_id"]}, {"$set":new_obj})
 
     @staticmethod
     async def update_one_user_reward_status(usr:dict,type:str, status:bool):
-        client = AsyncIOMotorClient(Database.uri, server_api=ServerApi('1'))
-        db = client.ttdb
+        db = await Database._get_db()
         await db[Collection.USER.value].update_many({"_id":usr._id},{"$set":{type:status}})
