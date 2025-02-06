7b1243c6d37249d6798aba7f62304ab4075c3d89
Vice <vincenzoassenza360@gmail.com>
2025-02-06 13:24:11 +0100

    fixed type

--- bot/discord/modules/admin/_admin.py
+++ bot/discord/modules/admin/_admin.py
@@ -362,26 +362,26 @@ class Admin(commands.Cog):
         names = ["LEADERBOARD.daily","LEADERBOARD.weekly","LEADERBOARD.monthly"]
         for i in range(3):
             msg:list[str] = ["","",""]
-            for us,rew,index in zip(lbs_npc[i],int(self.config[names[i]]),range(len(self.config[names[i]]))):
+            for us,rew,index in zip(lbs_npc[i],self.config[names[i]],range(len(self.config[names[i]]))):
                 if index == 0:
                     msg[0] += "**NPC**\n"
-                us["user"].ett += rew * (int(cnf2.value) if cnf2 is not None else 1)
+                us["user"].ett += int(rew) * (int(cnf2.value) if cnf2 is not None else 1)
                 await Database.update_user(us["user"].discord_id,us["user"].name,us["user"].ett,us["user"].btt,us["user"].daily,us["user"].weekly,us["user"].monthly)
-                msg[0] += f"#{index+1} {us["user"].ign} +{rew * (int(cnf2.value) if cnf2 is not None else 1)} ETT\n"
+                msg[0] += f"#{index+1} {us["user"].ign} +{int(rew) * (int(cnf2.value) if cnf2 is not None else 1)} ETT\n"
 
             for us,rew,index in zip(lbs_stp[i],self.config[names[i]],range(len(self.config[names[i]]))):
                 if index == 0:
                     msg[1] += "**Steps**\n"
-                us["user"].ett += rew * (int(cnf2.value) if cnf2 is not None else 1)
+                us["user"].ett += int(rew) * (int(cnf2.value) if cnf2 is not None else 1)
                 await Database.update_user(us["user"].discord_id,us["user"].name,us["user"].ett,us["user"].btt,us["user"].daily,us["user"].weekly,us["user"].monthly)
-                msg[1] += f"Step\n#{index+1} {us["user"].ign} +{rew * (int(cnf2.value) if cnf2 is not None else 1)} ETT\n"
+                msg[1] += f"Step\n#{index+1} {us["user"].ign} +{int(rew) * (int(cnf2.value) if cnf2 is not None else 1)} ETT\n"
 
             for us,rew,index in zip(lbs_pvp[i],self.config[names[i]],range(len(self.config[names[i]]))):
                 if index == 0:
                     msg[2] += "**PVP**\n"
-                us["user"].ett += rew * (int(cnf2.value) if cnf2 is not None else 1)
+                us["user"].ett += int(rew) * (int(cnf2.value) if cnf2 is not None else 1)
                 await Database.update_user(us["user"].discord_id,us["user"].name,us["user"].ett,us["user"].btt,us["user"].daily,us["user"].weekly,us["user"].monthly)
-                msg[2] += f"#{index+1} {us["user"].ign} +{rew * (int(cnf2.value) if cnf2 is not None else 1)} ETT\n"
+                msg[2] += f"#{index+1} {us["user"].ign} +{int(rew) * (int(cnf2.value) if cnf2 is not None else 1)} ETT\n"
             if cnf is not None:
                 try:
                     channel = await self.client.fetch_channel(int(cnf.value))
